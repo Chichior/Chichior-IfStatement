441299de0fbafa47fdbdc8b63088df7e9e4b1332
Chichior <katiehakala@icloud.com>
2023-01-27 01:55:38 +0000

    IfStatementComplete

--- src/main/java/IfStatement.java
+++ src/main/java/IfStatement.java
@@ -20,6 +20,14 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+            //Let's make an if statement that does the following:
+            if(bool == true){
+                //if bool is true, return x.
+                return x;
+            } else {  
+                //if bool is false, return y.
+                return y;
+            }
+        }
     }
-}
+
